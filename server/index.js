ccdc295fcca5c69d1d8f3170540c63af921fcf0f
xzhang <xinbo_zhang@163.com>
2019-07-03 16:51:49 +0800

    test:格式化 eslint-plugin-prettier

--- server/index.js
+++ server/index.js
@@ -1,38 +1,37 @@
-if (typeof window === "undefined") {
+if (typeof window === 'undefined') {
   global.window = {};
 }
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const { renderToString } = require("react-dom/server");
-const SSR = require("../dist/search-server");
-const template = fs.readFileSync(
-  path.join(__dirname, "../dist/search.html"),
-  "utf-8"
-);
-const data = require("./data.json");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { renderToString } = require('react-dom/server');
+const SSR = require('../dist/search-server');
 
+const template = fs.readFileSync(path.join(__dirname, '../dist/search.html'), 'utf-8');
+const data = require('./data.json');
+
+const renderMarup = str => {
+  const dataStr = JSON.stringify(data);
+  return template
+    .replace('<!--HTML_PLACEHOLDER-->', str)
+    .replace(
+      '<!--INITIAL_DATA_PLACEHOLDER-->',
+      `<script>window.__initial_data=${dataStr}</script>`
+    );
+};
 const server = port => {
   const app = express();
 
-  app.use(express.static("dist"));
+  app.use(express.static('dist'));
 
-  app.get("/search", (req, res) => {
+  app.get('/search', (req, res) => {
     const html = renderMarup(renderToString(SSR));
     res.status(200).send(html);
   });
 
   app.listen(port, () => {
-    console.log("server is runing on port" + port);
+    console.log(`server is runing on port${port}`);
   });
 };
+
 server(process.env.PORT || 3000);
-const renderMarup = str => {
-  const dataStr = JSON.stringify(data);
-  return template
-    .replace("<!--HTML_PLACEHOLDER-->", str)
-    .replace(
-      "<!--INITIAL_DATA_PLACEHOLDER-->",
-      `<script>window.__initial_data=${dataStr}</script>`
-    );
-};
